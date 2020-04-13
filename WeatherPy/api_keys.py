46d43c813dc542e626ffa0ee6e8853a42d1aeb2c
Maithri Mathew <maithreyi.mathew@gmail.com>
2020-04-13 13:58:27 -0400

    reattempted json conversion

--- WeatherPy/api_keys.py
+++ WeatherPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "06f81924f577b858e737a4556f10ab7c"
+weather_api_key ="06f81924f577b858e737a4556f10ab7c"
 
 # Google API Key
-g_key = "linen-bliss-274018"
+g_key ="AIzaSyD4N9jDQGEM_Q-T5gEpt4loR_PGhP4j1nU"
